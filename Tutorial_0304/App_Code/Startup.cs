30a01d712b990dacf7efc28eee1b9707e54b9833
Adrian Khor <adriank96@outlook.com>
2015-03-04 15:03:48 +0800

    Version 0.1

---
+++ Tutorial_0304/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tutorial_0304.Startup))]
+namespace Tutorial_0304
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
